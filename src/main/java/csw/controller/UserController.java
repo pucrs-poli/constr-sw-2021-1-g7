84697431eae77588e0d573a5345976af8304a0f1
Eduardo Dornelles <e_dornelles1992@hotmail.com>
2021-05-05 20:45:08 -0300

    WIP - configuracoes

--- src/main/java/csw/controller/UserController.java
+++ src/main/java/csw/controller/UserController.java
@@ -1,17 +1,9 @@
 package csw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import csw.dto.HttpResponseDTO;
-import csw.model.User;
 import csw.service.logic.UserService;
 
 @RestController
