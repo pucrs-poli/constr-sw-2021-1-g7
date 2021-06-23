d5228f6ffd5622ce9609d8a9a7a215505038d202
Eduardo Dornelles <e_dornelles1992@hotmail.com>
2021-06-23 19:54:42 -0300

    ajustes cors

--- src/main/java/csw/security/WebSecurity.java
+++ src/main/java/csw/security/WebSecurity.java
@@ -13,14 +13,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter implements Constan
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable().csrf().disable();
-//                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
-//                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
-//                // this disables session creation on Spring Security
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().disable().csrf().disable().authorizeRequests()
+        .antMatchers("/*").permitAll();
     }
 
   @Bean
